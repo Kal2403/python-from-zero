f7cec606d93d69c2f80890842e51fb5ec02ce5f9
Cristhian Castillo <kal2403199277@gmail.com>
2026-05-21 05:28:08 +0200

    built in function

---
+++ 02_Variables_builn_in_functions/variables.py
@@ -0,0 +1,46 @@
+# 30 Days Of Python
+
+first_name = 'Kal'
+last_name = 'Castillo'
+full_name = first_name + ' ' + last_name
+country = 'Spain'
+city = 'Pontevedra'
+age = 34
+year = 2026
+is_married = False
+is_true = True
+is_light_on = True
+
+print('First Name Data Type: ', type(first_name))
+print('Last Name Data Type: ', type(last_name))
+print('Full Name Data Type: ', type(full_name))
+print('Country Data Type: ', type(country))
+print('City Data Type: ', type(city))
+print('Age Data Type: ', type(age))
+print('Year Data Type: ', type(year))
+print('Is Married Data Type: ', type(is_married))
+print('Is True Data Type: ', type(is_true))
+print('Is Light On Data Type: ', type(is_light_on))
+
+print(len(first_name))
+
+print(len(first_name) == len(last_name))
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+diff = num_two - num_one
+product = num_two * num_one
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+print('Total: ', total)
+print('Difference: ', diff)
+print('Product: ', product)
+print('Division: ', division)
+print('Remainder: ', remainder)
+print('Exponent: ', exp)
+print('Floor Division: ', floor_division)
